df6ded77912777fbb4ca276ba6d273ddc9a9f86a
wanny.park <wanny.park@pandora.tv>
2024-03-26 17:50:01 +0900

    carthage 설정 추가

---
+++ sources/CodableCSV.h
@@ -0,0 +1,18 @@
+//
+//  CodableCSV.h
+//  CodableCSV
+//
+//  Created by manaes on 3/26/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CodableCSV.
+FOUNDATION_EXPORT double CodableCSVVersionNumber;
+
+//! Project version string for CodableCSV.
+FOUNDATION_EXPORT const unsigned char CodableCSVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodableCSV/PublicHeader.h>
+
+
